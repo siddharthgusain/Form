c0f5e13136846ff9c1f3c4b248cc5238b169c6c5
siddharthgusain <siddharthgusain26011999@gmail.com>
2019-03-19 21:17:13 +0530

    "created event"

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component{
+
+    onInputChangeHandler(event){
+       
+    }
+
+    
     render(){
         return(
-        <div>
-           <form>
-               <input type="text"/>
-           </form>
+        <div className="ui segment" >
+           <form className="ui form">
+                <div className="field">
+                    <label>Image Search</label>
+                    <input 
+                    type="text" 
+                    onChange={(e) =>  console.log(e.target.value) }
+                    />
+                </div>
+            </form>
         </div>
         );
     }
